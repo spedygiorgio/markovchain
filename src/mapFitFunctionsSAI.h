603faf650696d2674ae88d73cd0c2ec0361fef0f
ysaibhargav <bhargavcoolboy@gmail.com>
2015-06-14 23:14:21 +0530

    Changed Rcout to warning

--- src/mapFitFunctionsSAI.h
+++ src/mapFitFunctionsSAI.h
@@ -125,7 +125,7 @@ List _mcFitMap(CharacterVector stringchar, bool byrow, double confidencelevel, N
   outMc.slot("transitionMatrix") = mapEstMatr;
   outMc.slot("name") = "Bayesian Fit";  
   
-  Rcout << "\'estimate\' is the MAP set of parameters where as \'expectedValue\' \nis the expectation of the parameters with respect to the posterior\n";
+  warning("\n\'estimate\' is the MAP set of parameters where as \'expectedValue\' \nis the expectation of the parameters with respect to the posterior\n");
   
   return List::create(_["estimate"] = outMc,
     _["expectedValue"]=expMatr,

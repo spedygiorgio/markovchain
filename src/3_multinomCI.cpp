9533f53049e1e3f930477bd8de180a3788ea556d
tskang <tskang3@gmail.com>
2015-07-07 01:21:13 -0400

    Added truncpoi function

--- src/3_multinomCI.cpp
+++ src/3_multinomCI.cpp
@@ -4,7 +4,7 @@
 
 using namespace Rcpp;
 
-// double moments(c,lambda){
+NumericVector moments(int c, double lambda){
 // a=lambda+c;
 // b=lambda-c;
 // if(b<0){ 
@@ -18,9 +18,11 @@ using namespace Rcpp;
 // # den=poisson(lambda,a);
 // den=ppois(a,lambda);
 // }
-// mu=mat.or.vec(4,1); 
+// mu=mat.or.vec(4,1);
+  NumericVector mu(4);
 // # mom es global y se usa fuera de esta funci?n
-// mom=mat.or.vec(5,1); 
+// mom=mat.or.vec(5,1);
+  NumericVector mom(5);
 // for(r in 1:4){
 // poisA=0;
 // poisB=0;
@@ -36,39 +38,52 @@ using namespace Rcpp;
 // mom[3]=mu[3]+mu[2]*(3-3*mu[1])+(mu[1]-3*mu[1]^2+2*mu[1]^3);
 // mom[4]=mu[4]+mu[3]*(6-4*mu[1])+mu[2]*(7-12*mu[1]+6*mu[1]^2)+mu[1]-4*mu[1]^2+6*mu[1]^3-3*mu[1]^4;
 // mom[5]=den;
-// mom
-// }
+  return mom;
+}
+
+NumericVector colSums(NumericMatrix m) {
+  NumericVector out;
+  for(int i = 0; i < m.rows(); i ++) {
+    out.push_back(sum(m.row(i)));
+  }
+  return out;
+}
+
+double ppois(double n, double lambda) {
+  return R::ppois(n,lambda, true, false);
+}
 
 double truncpoi(int c, NumericVector x, double n, int k){
   NumericMatrix m(k,5);
   for(int i = 0; i < k; i ++){
     double lambda=x[i];
-//     NumericVector mom = moments(c,lambda);
-//     for(int j = 0; j < 5; j ++)
-//       m[i,j]=mom[j];
+    NumericVector mom = moments(c,lambda);
+    for(int j = 0; j < 5; j ++)
+      m(i,j)=mom[j];
   }
   for(int i = 0; i < k; i ++)
     m(i,3)=m(i,3)-3*m(i,1)*m(i,1);
-    
-//   NumericVector s=colSums(m);
-//   double s1=s[0];
-//   double s2=s[1];
-//   double s3=s[2];
-//   double s4=s[3];
-  //   
-  //   probn=1/(ppois(n,n)-ppois(n-1,n));
-  //   z=(n-s1)/sqrt(s2);
-  //   g1=s3/(s2^(3/2));
-  //   g2=s4/(s2^2);
-  //   poly=1+g1*(z^3-3*z)/6+g2*(z^4-6*z^2+3)/24
-  //   +g1^2*(z^6-15*z^4+45*z^2-15)/72;
-  //   f=poly*exp(-z^2/2)/(sqrt(2)*gamma(0.5));
-  //   probx=1;
-  //   for(i in 1:k){
-  //     probx=probx*m[i,5];
-  //   }
-  //   return(probn*probx*f/sqrt(s2));
-  return 0;
+  
+  // Rf_PrintValue(m); 
+  NumericVector s=colSums(m);
+  // Rf_PrintValue(s);
+  double s1=s[0];
+  double s2=s[1];
+  double s3=s[2];
+  double s4=s[3];
+  
+  double probn=1/(ppois(n,n)-ppois(n-1,n));
+  double z=(n-s1)/sqrt(s2);
+  double g1=s3/(pow(s2,(3/2)));
+  double g2=s4/(pow(s2,2));
+  double poly=1+g1*(pow(z,3)-3*z)/6+g2*(pow(z,4)-6*pow(z,2)+3)/24
+              +pow(g1,2)*(pow(z,6)-15*pow(z,4)+45*pow(z,2)-15)/72;
+  double f=poly*exp(-pow(z,2)/2)/(sqrt(2)*R::gammafn(0.5)); 
+  double probx=1;
+  for(int i = 0; i < k; i++){
+    probx=probx*m(i,4);
+  }
+  return(probn*probx*f/sqrt(s2));
 }
 
 NumericMatrix multinomialCIForRow (NumericVector x, double confidencelevel){

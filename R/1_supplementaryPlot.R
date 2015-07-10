dd055fcd319857eac1a9263f04f151a656e23898
tskang <tskang3@gmail.com>
2015-07-10 17:21:51 -0400

    Fixed DiagrammeR::add_edges warning

--- R/1_supplementaryPlot.R
+++ R/1_supplementaryPlot.R
@@ -1,68 +1,77 @@
 library(diagram)
 library(DiagrammeR)
 
-.plotdiagram <- function(object) {
+.plotdiagram <- function(object, options = NULL) {
   mat <- object@transitionMatrix
-  # print(mat)
-  # plotmat(mat)
-  res <- plotmat(mat, box.size = 0.06)
-#   curves <- matrix(nrow = ncol(mat), ncol = ncol(mat), 0)
-#   curves[3, 1] <- curves[1, 6] <- -0.35
-#   curves[4, 6] <- curves[6, 4] <- curves[5, 6] <- curves[6, 5] <- 0.08
-#   curves[3, 6] <- 0.35
-#   plotmat(mat, pos = c(3, 2, 1), curve = curves,
-#             name = colnames(mat), lwd = 1, box.lwd = 2,
-#             cex.txt = 0.8, box.cex = 0.8, box.size = 0.08,
-#             arr.length = 0.5, box.type = "circle", box.prop = 1,
-#             shadow.size = 0.01, self.cex = 0.6, my = -0.075, mx = -0.01,
-#             relsize = 0.9, self.shiftx = c(0, 0, 0.125, -0.12, 0.125, 0),
-#             self.shifty = 0, main = "Diagram")
+  res <- plotmat(mat, options)
   return (res)
 }
 
-.plotDiagrammeR <- function(object) {
+.plotDiagrammeR <- function(object, options = NULL) {
   mat <- object@transitionMatrix
+  nodes <- ''
+  for(i in 1:nrow(mat)) {
+    nodes<- paste0(nodes, i, "; ")
+  }
+  # print(nodes)
+  edges <- ''
+  for(i in 1:nrow(mat)) {
+    for(j in 1:ncol(mat)) {
+      # 1->1 [label = 0.80]
+      edges <- paste0(edges, i, "->", j, " [label = ", mat[i,j], "] ")
+    }
+  }
+  # print(edges)
+#   res <- DiagrammeR("
+#   graph LR;
+#              A(1)-->|0.5|B(2)
+#               A-->|0.5|A
+#               B-->|0.1|B
+#              B-->|0.3|C(3)
+#               C-->|0.9|B;
+#              
+#              style A fill:#A2EB86, stroke:#04C4AB, stroke-width:2px;
+#              style B fill:#FFF289, stroke:#FCFCFF, stroke-width:2px, stroke-dasharray: 4, 4;
+#              style C fill:#FFA070, stroke:#FF5E5E, stroke-width:2px;
+#              ")
   
-  res <- DiagrammeR("
-  graph LR;
-             A(1)-->|0.5|B(2)
-              A-->|0.5|A
-              B-->|0.1|B
-             B-->|0.3|C(3)
-              C-->|0.9|B;
-             
-             style A fill:#A2EB86, stroke:#04C4AB, stroke-width:2px;
-             style B fill:#FFF289, stroke:#FCFCFF, stroke-width:2px, stroke-dasharray: 4, 4;
-             style C fill:#FFA070, stroke:#FF5E5E, stroke-width:2px;
-             ")
-  
-  res <- grViz("
+  res <- grViz(paste0("
   digraph circles {
-        
         # a 'graph' statement
         graph [overlap = true, fontsize = 10]
-        
-        # several 'node' statements
-#         node [shape = box,
-#         fontname = Helvetica]
-#         A; B; C; D; E; F
-        
+
         node [shape = circle,
         fixedsize = true,
         width = 0.9] // sets as circles
-        1; 2; 3; 4; #5; 6; 7; 8
+        ", nodes, "
         
         # several 'edge' statements
-        1->1 [label = 0.80] 1->2 [label = 0.1] 1->3 [label = 0.1] 
-        2->1 [label = 0.75] 2->2 [label = 0.25]
-        3->2 [label = 0.5] 3->3 [label='0.4'] 3->4 [label = 0.1]
-        4->4 [label = 1.0] 
-        
-        # A->1 B->2 B->3 B->4 C->A
-#         1->D E->A 2->4 1->5 1->F
-#         E->6 4->6 5->7 6->7 3->8
+        ", edges, " 
   }
-  ")
+  "))
+  
+#   res <- grViz("
+#   digraph circles {
+#         # a 'graph' statement
+#         graph [overlap = true, fontsize = 10]
+#         
+#         # several 'node' statements
+# #         node [shape = box,
+# #         fontname = Helvetica]
+# #         A; B; C; D; E; F
+#         
+#         node [shape = circle,
+#         fixedsize = true,
+#         width = 0.9] // sets as circles
+#         1; 2; 3; 4; 
+#         
+#         # several 'edge' statements
+#         1->1 [label = 0.80] 1->2 [label = 0.1] 1->3 [label = 0.1] 
+#         2->1 [label = 0.75] 2->2 [label = 0.25]
+#         3->2 [label = 0.5] 3->3 [label='0.4'] 3->4 [label = 0.1]
+#         4->4 [label = 1.0] 
+#   }
+#   ")
   return (res)
 }
 
--- tests/testthat/testPlot.R
+++ tests/testthat/testPlot.R
@@ -37,3 +37,15 @@ mcWeather
 # plot(mcWeather)
 .plotdiagram(mcWeather)
 .plotDiagrammeR(mcWeather)
+
+#   curves <- matrix(nrow = ncol(mat), ncol = ncol(mat), 0)
+#   curves[3, 1] <- curves[1, 6] <- -0.35
+#   curves[4, 6] <- curves[6, 4] <- curves[5, 6] <- curves[6, 5] <- 0.08
+#   curves[3, 6] <- 0.35
+#   plotmat(mat, pos = c(3, 2, 1), curve = curves,
+#             name = colnames(mat), lwd = 1, box.lwd = 2,
+#             cex.txt = 0.8, box.cex = 0.8, box.size = 0.08,
+#             arr.length = 0.5, box.type = "circle", box.prop = 1,
+#             shadow.size = 0.01, self.cex = 0.6, my = -0.075, mx = -0.01,
+#             relsize = 0.9, self.shiftx = c(0, 0, 0.125, -0.12, 0.125, 0),
+#             self.shifty = 0, main = "Diagram")

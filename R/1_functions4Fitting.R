18676d21b847a7258cf21fb1e3477177bd04288e
tskang <tskang3@gmail.com>
2015-08-09 00:06:28 -0400

    .fitHigherOrder

--- R/1_functions4Fitting.R
+++ R/1_functions4Fitting.R
@@ -243,24 +243,68 @@ rmarkovchain<-function(n,object,...)
   return(outMc)
 }
 
+.rotate=function(x, n) {
+  l=length(x)
+  n=n %% l
+  if (n == 0) {
+    return(x)
+  }
+  tmp=x[(l-n+1):l]
+  x[(n+1):l]=x[1:(l-n)]
+  x[1:n]=tmp
+  return(x)
+}
+
+.getQ=function(i, clickstreamList) { 
+  clicks=clickstreamList
+  for (j in 1:i) {
+    clicks=rbind(clicks, "[[-]]")
+  }
+  clicks=unlist(clicks, use.names=F)
+  clicks2=.rotate(clicks, -i)
+  dat=data.table(clicks, clicks2) 
+  transition=as.data.frame(dcast.data.table(dat, clicks~clicks2, fun.aggregate=length, value.var="clicks2"))
+  transition=transition[,-1]  
+  pos=which(names(transition)=="[[-]]")
+  rnames=names(transition)[-pos]
+  transition=transition[,-pos]
+  transition=transition[-pos,]
+  sums=colSums(t(transition))
+  sums[sums==0]=1
+  ll=sum(transition*log(transition/sums), na.rm=T)
+  transition=as.data.frame(t(transition/sums))
+  names(transition)=rnames
+  rownames(transition)=rnames
+  return(list(ll=ll, transition=transition))
+}
+
+.foo=function(params) {
+  QX=get("QX")
+  X=get("X")    
+  error=0
+  for (i in 1:length(QX)) {
+    error=error+(params[i]*QX[[i]]-X)
+  }
+  return(sum(error^2))
+}
+
+.constr=function(params) {
+  return(sum(params))
+}
 
 #fit
-.fitHigherOrder<-function(data, order) {
-  X=as.numeric(x$probability)
-  ll=vector()
-  Q=alply(.data=seq(1,order,1), .margins=1, .fun=.getQ, clickstreamList)
+.fitHigherOrder<-function(frequency, sequencelist, order) {
+  X=as.numeric(frequency/sum(frequency))
+  # ll=vector()
+  Q=alply(.data=seq(1,order,1), .margins=1, .fun=.getQ, sequencelist)
   transitions=llply(.data=Q, .fun=function(q) q$transition)
-  ll=laply(.data=Q, .fun=function(q) q$ll)
+  # ll=laply(.data=Q, .fun=function(q) q$ll)
   QX=llply(.data=transitions, .fun=function(tr) as.matrix(tr)%*%X) 
   environment(.foo)=environment()
-  if (ans$optimizer == "quadratic") {
-    params=rep(1/order, order)
-    model=solnp(pars=params, fun=.foo, eqfun=.constr, eqB=1, LB=rep(0, order), control=list(trace=0))
-    lambda=model$pars
-  } else {
-    solution=.sollp(X, QX, ans$use.lpSolve)
-    lambda=solution[seq(length(solution) - length(QX) + 1, length(solution), 1)]
-  }
+  params=rep(1/order, order)
+  model=solnp(pars=params, fun=.foo, eqfun=.constr, eqB=1, LB=rep(0, order), control=list(trace=0))
+  lambda=model$pars
+  return(lambda)
 }
 
 #markovchainFit <- function(data, method = "mle", byrow = TRUE, nboot = 10L, laplacian = 0, name = "", parallel = FALSE, confidencelevel = 0.95) {

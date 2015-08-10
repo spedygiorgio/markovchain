a4ec060508a342cf8a6ea20600b058dbae636f51
tskang <tskang3@gmail.com>
2015-08-10 15:48:16 -0400

    Small fix

--- R/1_functions4Fitting.R
+++ R/1_functions4Fitting.R
@@ -263,19 +263,19 @@ rmarkovchain<-function(n,object,...)
   clicks=unlist(clicks, use.names=F)
   clicks2=.rotate(clicks, -i)
   dat=data.table(clicks, clicks2) 
-  transition=as.data.frame(dcast.data.table(dat, clicks~clicks2, fun.aggregate=length, value.var="clicks2"))
-  transition=transition[,-1]  
-  pos=which(names(transition)=="[[-]]")
-  rnames=names(transition)[-pos]
-  transition=transition[,-pos]
-  transition=transition[-pos,]
-  sums=colSums(t(transition))
-  sums[sums==0]=1
-  ll=sum(transition*log(transition/sums), na.rm=T)
-  transition=as.data.frame(t(transition/sums))
-  names(transition)=rnames
-  rownames(transition)=rnames
-  return(list(ll=ll, transition=transition))
+#   transition=as.data.frame(dcast.data.table(dat, clicks~clicks2, fun.aggregate=length, value.var="clicks2"))
+#   transition=transition[,-1]  
+#   pos=which(names(transition)=="[[-]]")
+#   rnames=names(transition)[-pos]
+#   transition=transition[,-pos]
+#   transition=transition[-pos,]
+#   sums=colSums(t(transition))
+#   sums[sums==0]=1
+#   ll=sum(transition*log(transition/sums), na.rm=T)
+#   transition=as.data.frame(t(transition/sums))
+#   names(transition)=rnames
+#   rownames(transition)=rnames
+#   return(list(ll=ll, transition=transition))
 }
 
 .foo=function(params) {
@@ -296,10 +296,10 @@ rmarkovchain<-function(n,object,...)
 .fitHigherOrder<-function(frequency, sequencelist, order) {
   X=as.numeric(frequency/sum(frequency))
   # ll=vector()
-  Q=alply(.data=seq(1,order,1), .margins=1, .fun=.getQ, sequencelist)
-  transitions=llply(.data=Q, .fun=function(q) q$transition)
+  # Q=alply(.data=seq(1,order,1), .margins=1, .fun=.getQ, sequencelist)
+  # transitions=llply(.data=Q, .fun=function(q) q$transition)
   # ll=laply(.data=Q, .fun=function(q) q$ll)
-  QX=llply(.data=transitions, .fun=function(tr) as.matrix(tr)%*%X) 
+  # QX=llply(.data=transitions, .fun=function(tr) as.matrix(tr)%*%X) 
   environment(.foo)=environment()
   params=rep(1/order, order)
   model=solnp(pars=params, fun=.foo, eqfun=.constr, eqB=1, LB=rep(0, order), control=list(trace=0))

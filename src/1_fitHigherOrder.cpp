facda85423b69597e40b4f6e6fd3bc774b94f26a
tskang <tskang3@gmail.com>
2015-08-19 17:34:35 -0400

    solnp

--- src/1_fitHigherOrder.cpp
+++ src/1_fitHigherOrder.cpp
@@ -1,4 +1,5 @@
-#include <Rcpp.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
 using namespace Rcpp;
 
 // sequence to frequency probability vector
@@ -21,7 +22,7 @@ NumericMatrix seq2matHigh(CharacterVector sequence, int order) {
   int n = sequence.size();
   CharacterVector states = unique(sequence).sort();
   int nstates = states.length();
-  
+  NumericVector colsums(nstates);
   NumericMatrix out(nstates);
   out.attr("dimnames") = List::create(states, states);
   for(int i = 0; i < n - order; i ++) {
@@ -30,27 +31,22 @@ NumericMatrix seq2matHigh(CharacterVector sequence, int order) {
       if(sequence[i] == states[j]) from = j;
       if(sequence[i + order] == states[j]) to = j;
     }
-    if(from != -1 && to != -1)
+    if(from != -1 && to != -1) {
       out(to, from) ++;
-    // out(to, from) = out(to, from) + 1.0;
+      colsums[from] ++;
+    }
   }
   // Rf_PrintValue(out);
+  for(int i = 0; i < nstates; i ++) {
+    for(int j = 0; j < nstates; j ++)
+      out(i, j) /= colsums[j];
+  }
   return out;
 }
-// .seq2matHigh<-function(sequence, order) {
-// n<-length(sequence)
-//   states<-unique(sequence)
-//   nstates<-length(states)
-//   mat<-zeros(nstates)
-//   dimnames(mat)<-list(states, states)
-//   for(i in 1:(n-order)) {
-//     from<-sequence[i]
-//     to<-sequence[i+order]
-//     mat[to,from]<-mat[to,from]+1
-//   }
-//   return (mat)
-// }
-// 
+
+void fn1() {
+  
+}
 // .fn1=function(params)
 // {
 //   QX=get("QX")
@@ -65,45 +61,27 @@ NumericMatrix seq2matHigh(CharacterVector sequence, int order) {
 // .eqn1=function(params){
 // return(sum(params))
 // }
-// 
-// fitHigherOrder<-function(sequence, order = 2) {
-//   X<-.seq2freqProb(sequence)
-//   F<-list()
-//   Q<-list()
-//   QX<-list()
-//   for(o in 1:order) {
-//     F[[o]]<-.seq2matHigh(sequence, o)
-//     Q[[o]]<-sweep(F[[o]], 2, colSums(F[[o]]), '/')
-//     QX[[o]]<-Q[[o]]%*%X
-// # print(QX[[o]])
-//   }
-//   environment(.fn1)=environment()
-//     params<-rep(1/order, order)
-//     model<-Rsolnp::solnp(params, fun = .fn1, eqfun = .eqn1, eqB=1, LB=rep(0, order), control=list(trace=0))
-// # model<-Rsolnp::solnp(x0, fun = .fn1, eqfun = .eqn1, eqB = c(10, 0, -1), control=list(trace=0))
-// # print(model)
-// #   model=Rsolnp::solnp(pars=params, fun=.foo, eqfun=.constr, eqB=1, LB=rep(0, order), control=list(trace=0))
-//     lambda=model$pars
-//     return(lambda)
-// }
 
 // [[Rcpp::export]]
 void fitHigherOrderRcpp(SEXP sequence, int order = 2) {
-  Rcout << "fitHigherOrder " << order << std::endl;
-  // Rf_PrintValue(sequence);
-  NumericVector X = seq2freqProb(sequence);
-  // Rf_PrintValue(X);
-  List F(order), Q(order), QX(order);
+  NumericVector v = seq2freqProb(sequence);
+  arma::vec X(v.begin(), v.size(), false);
+  // Rcout << X << std::endl;
+  List Q(order), QX(order);
   for(int i = 0; i < order; i ++) {
-    F[i] = seq2matHigh(sequence, i + 1);
+    NumericMatrix Qi = seq2matHigh(sequence, i + 1);
+    arma::mat m(Qi.begin(), Qi.nrow(), Qi.ncol(), false);
+    Q[i] = m;
+    QX[i] = m * X;
   }
-  // Rf_PrintValue(F);
-  //   for(o in 1:order) {
-  //     F[[o]]<-.seq2matHigh(sequence, o)
-  //     Q[[o]]<-sweep(F[[o]], 2, colSums(F[[o]]), '/')
-  //     QX[[o]]<-Q[[o]]%*%X
-  // # print(QX[[o]])
-  //   }
+  // Rf_PrintValue(Q);
+  // Rf_PrintValue(QX);
+  Environment env;
+  env[".fn1"] = 1;
+  Rcout << env << std::endl;
+  Function solnp("solnp");
+  NumericVector params;
+  // List res = solnp(params, Named("fun", fn1));
   //   environment(.fn1)=environment()
   //     params<-rep(1/order, order)
   //     model<-Rsolnp::solnp(params, fun = .fn1, eqfun = .eqn1, eqB=1, LB=rep(0, order), control=list(trace=0))

d543ab05ab250fa7a1c5eea89e598eb66b2b9f2d
Giorgio Alfredo Spedicato <spedygiorgio@gmail.com>
2016-06-25 02:37:36 +0200

    added demo for fundamental matrix

---
+++ demo/extractMatrices.R
@@ -0,0 +1,26 @@
+#function to extract matrices 
+extractMatrices <- function(mcObj) {
+  require(matlab)
+  mcObj <- canonicForm(object = mcObj)
+  #get the indices of transient and absorbing
+  transIdx <- which(states(mcObj) %in% transientStates(mcObj))
+  absIdx <- which(states(mcObj) %in% absorbingStates(mcObj))
+  #get Q, R and I
+  Q <- as.matrix(mcObj@transitionMatrix[transIdx,transIdx])
+  R <- as.matrix(mcObj@transitionMatrix[transIdx,absIdx])
+  I <- as.matrix(mcObj@transitionMatrix[absIdx, absIdx])
+  #get fundamental matrix
+  N <- solve(eye(size(Q)) - Q)
+  #final absorbion probabilities
+  NR <- N %*% R
+  #return
+  out <- list(
+    canonicalForm = mcObj,
+    Q = Q,
+    R = R,
+    I = I, 
+    N=N,
+    NR=NR
+  )
+  return(out)
+}

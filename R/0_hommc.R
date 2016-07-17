5f1ad7ef48d8bd63d54a640570793a4fc1782123
cryptomanic <deepakyadav.iitbhu@gmail.com>
2016-07-17 15:54:04 +0530

    show method hommc

--- R/0_hommc.R
+++ R/0_hommc.R
@@ -28,3 +28,51 @@ hommc <- setClass("hommc",
                       P = "array", Lambda = "numeric", byrow = "logical",
                       name = "character")
 )
+
+# internal method to show hommc object in informative way
+.showHommc <- function(object) {
+  
+  # whether data in transition matrices are stored in column-wise or row-wise fashion
+  if(object@byrow == TRUE) {
+    direction <- "(by rows)" 
+  } else {
+    direction <- "(by cols)" 
+  }
+  
+  # display order and unique states
+  cat("Order of multivariate markov chain =", object@order, "\n")
+  cat("states =", object@states, "\n")
+  
+  cat("\n")
+  cat("List of Lambda's and the corresponding transition matrix", direction,":\n")
+  
+  # display transition matrices and the corresponding lambdas
+  n <- object@order
+  s <- sqrt((dim(object@P))[3]/n)
+  
+  for(i in 1:s) {
+    for(j in 1:s) {
+      t <- n*s*(i-1) + (j-1)*n
+      for(k in 1:n) {
+        cat("Lambda", k, "(", i, ",", j, ") : ", object@Lambda[t+k],"\n", sep = "")
+        cat("P", k, "(", i, ",", j, ") : \n", sep = "")
+        print(object@P[, , t+k])
+        cat("\n")
+      }  
+    }
+  }
+}
+
+#' @title Function to display the details of hommc object
+#' @description This is a convenience function to display the slots of hommc object
+#'              in proper format
+#' 
+#' @param object An object of class hommc
+#' 
+#' @rdname hommc-show
+#' @export                             
+setMethod("show", "hommc",
+          function(object){
+            .showHommc(object)
+          }
+)

373ac058cb8ffd663c3fdbe5437f16e4d7eebfac
Giorgio Alfredo Spedicato <Giorgio Alfredo Spedicato>
2017-02-18 21:45:20 +0100

    modified verify markov property

--- R/5_statisticalTests.R
+++ R/5_statisticalTests.R
@@ -1,3 +1,26 @@
+#helper functions
+
+
+#helper function for checkMP
+
+.findNijPjk<-function(Nijk=Nijk, Nij=Nij, trans=transMatrix, row=1){
+  i<-Nijk[row,1]
+  j<-Nijk[row,2]
+  k<-Nijk[row,3]
+  
+  fromCh<-as.character(j)
+  toCh<-as.character(k)
+  Pjk<-trans[fromCh,toCh]
+  
+  m1<-which(Nij[,1]==i)
+  m2<-which(Nij[,2]==j)
+  m<-c(m1,m2)
+  return(Nij[m[anyDuplicated(m)],3]*Pjk)
+}
+
+
+
+
 #' @name verifyMarkovProperty
 #' 
 #' @rdname verifyMarkovProperty
@@ -35,86 +58,126 @@ NULL
 #' @export
 
 # check if the sequence holds the Markov property
-verifyMarkovProperty <- function(sequence, ...) {
-  n <- length(sequence)
-  u <- unique(sequence)
-  stateNames <- u
-  nelements <- length(stateNames)
-  mat <- matlab::zeros(nrow = nelements, ncol = 3)
+verifyMarkovProperty <- function(sequence) {
   
-  # SSO: state sequence occurrences
-  # TSO: two state occurences
-  dimnames(mat) <- list(stateNames, c("SSO", "TSO", "TSO-SSO"))
+  #fitting the markovchain
   
-  # numeric vector initialized with zero for all states
-  SSO <- numeric()
-  for(i in 1:nelements) {
-    sname <- stateNames[i]
-    SSO[sname] <- 0
+  transMatrix<-markovchainFit(data=sequence)$estimate@transitionMatrix
+  
+  #make the n-2x3 matrix for observations
+  subSample<-sequence[1:(length(sequence)-(length(sequence)%%3))]
+  
+  seqSet1<-matrix(c(subSample[1:(length(subSample)-2)],
+                    subSample[2:(length(subSample)-1)],
+                    subSample[3:(length(subSample))]
+  ),ncol=3) #fill the matrix in reverse order so position 11 is the first obersvation,12 second and 13 third
+  #compute row frequencies
+  temp<-as.data.frame(seqSet1)
+  Nijk<-aggregate(temp, by=temp, length)[1:(ncol(temp)+1)]
+  
+  seqSet2<-seqSet1[,-3] #make matrix of couples
+  temp2<-as.data.frame(seqSet2)
+  Nij<-aggregate(temp2, by=temp2, length)[1:(ncol(temp2)+1)] #rowfrequencies included
+  
+  
+  test<-c(length=dim(Nijk)[1])
+  #compute the test statistic
+  for(z in 1:dim(Nijk)[1]){
+    foundNijPjk<-.findNijPjk(Nijk=Nijk, Nij=Nij, trans=transMatrix, row=z)
+    test[z]<-((Nijk[z,4]-foundNijPjk)^2)/foundNijPjk
   }
+  result<-sum(test)
+  #return value of the test statistic and test at confience level 95% and 99%
   
-  # numeric vector initialized with zero for all states
-  TSO <- SSO
+  #dof
   
-  # store the output to be returned
-  out <- list()
+  dof=length(unique(sequence))^3
   
-  for(present in stateNames) {
-    for(future in stateNames) {
-      
-      for(i in 1:nelements) {
-        TSO[i] <- SSO[i] <- 0  
-      }
-      
-      # populate TSO and SSO vector
-      for(i in 1:(n-1)) {
-        # let the ith state as past state
-        past <- sequence[i]
-        
-        # if next state in the sequence is present state
-        if(sequence[i+1] == present) {
-          TSO[past] <- TSO[past] + 1
-          
-          # if next to next state in the sequence is future state
-          if((i < n - 1) && (sequence[i+2] == future)) {
-            SSO[past] <- SSO[past] + 1
-          }
-        }
-      }
-      
-      # populate the matrix
-      # first column corresponds to SSO, second to TSO and
-      # third to their difference
-      
-      for(i in 1:(length(SSO))) {
-        mat[i, 1] <- SSO[i]
-        mat[i, 2] <- TSO[i]
-        mat[i, 3] <- TSO[i] - SSO[i]
-      }
-      
-    }
-  }
-      
-      # chi-squared test
-      
-      # between SSO and TSO-SSO
-      table <- as.data.frame(mat[, c(1, 3)])
-      
-      # an object of class htest
-      res <- chisq.test(table)
-      
-      # extract all information from htest object
-      # and stored the result in the form of list
-      res <- c(res)
-      
-      # SSO and TSO
-      table <- as.data.frame(mat[ , c(1, 2)])
-      
-      # stored the table in the list
-      res[["table"]] <- table
-      
-      # store the result corresponding to present state and future state
-      out[[paste0(present, future)]] <- res
+  
+  pvalue <- 1-pchisq(q = result,df = dof)
+  
+  out<-list(statistic=result,p.value=pvalue)
+  
+  # previous version
+  # n <- length(sequence)
+  # u <- unique(sequence)
+  # stateNames <- u
+  # nelements <- length(stateNames)
+  # mat <- matlab::zeros(nrow = nelements, ncol = 3)
+  # 
+  # # SSO: state sequence occurrences
+  # # TSO: two state occurences
+  # dimnames(mat) <- list(stateNames, c("SSO", "TSO", "TSO-SSO"))
+  # 
+  # # numeric vector initialized with zero for all states
+  # SSO <- numeric()
+  # for(i in 1:nelements) {
+  #   sname <- stateNames[i]
+  #   SSO[sname] <- 0
+  # }
+  # 
+  # # numeric vector initialized with zero for all states
+  # TSO <- SSO
+  # 
+  # # store the output to be returned
+  # out <- list()
+  # 
+  # for(present in stateNames) {
+  #   for(future in stateNames) {
+  #     
+  #     for(i in 1:nelements) {
+  #       TSO[i] <- SSO[i] <- 0  
+  #     }
+  #     
+  #     # populate TSO and SSO vector
+  #     for(i in 1:(n-1)) {
+  #       # let the ith state as past state
+  #       past <- sequence[i]
+  #       
+  #       # if next state in the sequence is present state
+  #       if(sequence[i+1] == present) {
+  #         TSO[past] <- TSO[past] + 1
+  #         
+  #         # if next to next state in the sequence is future state
+  #         if((i < n - 1) && (sequence[i+2] == future)) {
+  #           SSO[past] <- SSO[past] + 1
+  #         }
+  #       }
+  #     }
+  #     
+  #     # populate the matrix
+  #     # first column corresponds to SSO, second to TSO and
+  #     # third to their difference
+  #     
+  #     for(i in 1:(length(SSO))) {
+  #       mat[i, 1] <- SSO[i]
+  #       mat[i, 2] <- TSO[i]
+  #       mat[i, 3] <- TSO[i] - SSO[i]
+  #     }
+  #     
+  #   }
+  # }
+  #     
+  #     # chi-squared test
+  #     
+  #     # between SSO and TSO-SSO
+  #     table <- as.data.frame(mat[, c(1, 3)])
+  #     
+  #     # an object of class htest
+  #     res <- chisq.test(table)
+  #     
+  #     # extract all information from htest object
+  #     # and stored the result in the form of list
+  #     res <- c(res)
+  #     
+  #     # SSO and TSO
+  #     table <- as.data.frame(mat[ , c(1, 2)])
+  #     
+  #     # stored the table in the list
+  #     res[["table"]] <- table
+  #     
+  #     # store the result corresponding to present state and future state
+  #     out[[paste0(present, future)]] <- res
   
   
   return(out)

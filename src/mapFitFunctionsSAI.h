9d20e3c019d828295fdfa250bf07911a4dfbfe15
Giorgio Alfredo Spedicato <Giorgio Alfredo Spedicato>
2017-03-19 22:38:20 +0100

    suppressed warning

--- src/mapFitFunctionsSAI.h
+++ src/mapFitFunctionsSAI.h
@@ -201,7 +201,8 @@ List _mcFitMap(CharacterVector stringchar, bool byrow, double confidencelevel, N
   outMc.slot("transitionMatrix") = mapEstMatr;
   outMc.slot("name") = "Bayesian Fit";  
   
-  warning("\n\'estimate\' is the MAP set of parameters where as \'expectedValue\' \nis the expectation of the parameters with respect to the posterior.\nThe confidence intervals are given for \'estimate\'.");
+ // message("\n\'estimate\' is the MAP set of parameters where as \'expectedValue\' \nis the expectation 
+ // of the parameters with respect to the posterior.\nThe confidence intervals are given for \'estimate\'.");
   
   return List::create(_["estimate"] = outMc,
                       _["expectedValue"] = expMatr,

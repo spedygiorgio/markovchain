a80791b96e44691b652813d303d2210468bd9e71
spedygiorgio <spedygiorgio@gmail.com>
2017-05-06 23:34:49 +0200

    start roxygenizing

--- R/data.R
+++ R/data.R
@@ -16,3 +16,22 @@
 #' # fitHighOrderMultivarMC(seqMat = sales, order = 2, Norm = 2)
 #' 
 "sales"
+#' @name blanden
+#' 
+#' @title Mobility between income quartiles
+#' 
+#' @description This table show mobility between income quartiles for father and sons for the 1970 cohort born
+#' 
+#' @usage data(blanden)
+#' 
+#' @details The rows represent fathers' income quartile when the son is aged 16, whilst the columns represent sons' income quartiles when he is aged 30 (in 2000).
+#' 
+#' @source Personal reworking
+#' 
+#' @references Jo Blanden, Paul Gregg and Stephen Machin, Intergenerational Mobility in Europe and North America, Center for Economic Performances (2005)
+#' 
+#' @example 
+#' data(blanden)
+#' mobilityMc<-as(blanden, "markovchain")
+#' 
+"blanden"

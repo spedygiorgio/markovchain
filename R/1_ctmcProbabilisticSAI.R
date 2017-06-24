18880c613a001ed7c99ea802ac4cff4968523edb
vandit15 <jainvandit15@gmail.com>
2017-06-24 20:32:04 +0530

    minor change

--- R/1_ctmcProbabilisticSAI.R
+++ R/1_ctmcProbabilisticSAI.R
@@ -198,7 +198,7 @@ ExpectedTime <- function(C,i,j,useRCpp = TRUE){
 #' gen <- matrix(data = c(-1, 1/2, 1/2, 0, 1/4, -1/2, 0, 1/4, 1/6, 0, -1/3, 1/6, 0, 0, 0, 0),
 #' nrow = 4,byrow = byRow, dimnames = list(states,states))
 #' ctmc <- new("ctmc",states = states, byrow = byRow, generator = gen, name = "testctmc")
-#' probabilityatT(ctmc,1,useRcpp = TRUE)
+#' probabilityatT(ctmc,1,useRCpp = TRUE)
 #' 
 #' @export
 probabilityatT <- function(C,t,x0,useRCpp = TRUE){

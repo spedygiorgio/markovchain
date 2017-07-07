64571ad28801a9201e571f304f2be870634fefe3
vandit15 <jainvandit15@gmail.com>
2017-07-07 22:03:37 +0530

    adds examples to freq2Generator function

--- R/1_ctmcProbabilisticSAI.R
+++ R/1_ctmcProbabilisticSAI.R
@@ -89,6 +89,11 @@ transition2Generator<-function(P, t=1,method="logarithm") {
 #' sample_rel = rbind((sample/rowSums(sample))[1:dim(sample)[1]-1,],c(rep(0,dim(sample)[1]-1),1)) 
 #' freq2Generator(sample_rel,1)
 #' 
+#' data(tm_abs)
+#' tm_rel=rbind((tm_abs/rowSums(tm_abs))[1:7,],c(rep(0,7),1))
+#' ## Derive quasi optimization generator matrix estimate
+#' freq2Generator(tm_rel,1)
+#' 
 #' @export
 #' 
 freq2Generator <- function(P,t = 1,method = "QO",logmethod = "Eigen"){

0d8bbaac7db8b18b96166d2575d050ff8838f817
spedygiorgio <spedygiorgio@gmail.com>
2017-07-09 19:26:39 +0200

    added empirical bayes estimate

--- demo/quasiBayesian.R
+++ demo/quasiBayesian.R
@@ -1,12 +1,31 @@
 #PSEUDO BAYESIAN ESTIMATOR
 
-
-mcFitPseudoBayesian<-function(data, apriori) {
-  rawTransitionMatrix<-createSequenceMatrix(stringchar = data)
-  #obtain the colsums
-  vi<-rowSums(rawTransitionMatrix)
-  yij2<- rawTransitionMatrix^2
+getAlphaVector <- function(raw, apriori){
+  v_i <- rowSums(raw) 
+  K_i <- numeric(nrow(raw))
+  sumSquaredY <- rowSums(raw^2)
+  #get numerator
+  K_i_num <- v_i^2-sumSquaredY
+  #get denominator
+  VQ <- matrix(0,nrow= nrow(apriori),ncol=ncol(apriori))
+  for (i in 1:nrow(VQ)) {
+    VQ[i,]<-v_i[i]*apriori[i,]
+  }
+  
+  K_i_den<-rowSums((raw - VQ)^2)
+  
+  K_i <- K_i_num/K_i_den
+  
+  #get the alpha vector
+  alpha <- K_i / (v_i+K_i)
+  
+  #empirical transition matrix
+  Emp<-raw/rowSums(raw)
+  
+  #get the estimate
+  out<-matrix(0, nrow= nrow(raw),ncol=ncol(raw))
+  for (i in 1:nrow(out)) {
+    out[i,]<-alpha[i]*apriori[i,]+(1-alpha[i])*Emp[i,]
+  }
+  return(out)
 }
-
-apriori=as(matrix(c(0.5,0.5,0.5,0.5),byrow=TRUE,ncol=2),"markovchain")
-names(apriori)<-c("a","b")

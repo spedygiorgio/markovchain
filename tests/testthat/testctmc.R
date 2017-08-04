25239f06ec5f39f3ef93cd2665eb675fb681bd69
vandit15 <jainvandit15@gmail.com>
2017-08-04 17:02:56 +0530

    updates vignette and unit testing

--- tests/testthat/testctmc.R
+++ tests/testthat/testctmc.R
@@ -73,3 +73,18 @@ test_that("Check if ",{
   expect_equal(round(freq2Generator(sample_rel,1),3),answer)
 })
 
+### tests for is.CTMCirreducible fcuntion
+
+energyStates <- c("sigma", "sigma_star")
+byRow <- TRUE
+gen <- matrix(data = c(-3, 3,
+                       1, -1), nrow = 2,
+              byrow = byRow, dimnames = list(energyStates, energyStates))
+molecularCTMC <- new("ctmc", states = energyStates, 
+                     byrow = byRow, generator = gen, 
+                     name = "Molecular Transition Model")
+
+test_that("is.CTMCirreducible works", {
+  expect_equal(is.CTMCirreducible(molecularCTMC),TRUE)
+})
+

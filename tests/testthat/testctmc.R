7811d7174d7336b04a1ff5afa4d71c66cf1f6f87
vandit15 <jainvandit15@gmail.com>
2017-08-08 12:13:15 +0530

    adds vignette and unit testing for is.TimeReversible

--- tests/testthat/testctmc.R
+++ tests/testthat/testctmc.R
@@ -88,3 +88,20 @@ test_that("is.CTMCirreducible works", {
   expect_equal(is.CTMCirreducible(molecularCTMC),TRUE)
 })
 
+
+### tests for is.TimeReversible function
+
+energyStates <- c("sigma", "sigma_star")
+byRow <- TRUE
+gen <- matrix(data = c(-3, 3,
+                       1, -1), nrow = 2,
+              byrow = byRow, dimnames = list(energyStates, energyStates))
+molecularCTMC <- new("ctmc", states = energyStates, 
+                     byrow = byRow, generator = gen, 
+                     name = "Molecular Transition Model")
+
+test_that("is.TimeReversible works", {
+  expect_equal(is.TimeReversible(molecularCTMC),TRUE)
+})
+
+

764e44f51f8598c62c8c20b6a5a5013909104b01
ncordon <nacho.cordon.castillo@gmail.com>
2019-01-24 03:05:53 +0100

    Fixes random generation

--- R/random.R
+++ R/random.R
@@ -4,13 +4,16 @@ normalizeMatrix <- function(matrix, byrow = TRUE) {
   margin <- ifelse (byrow, 1, 2)
   n <- nrow(matrix)
   
-  result <- apply(matrix, MARGIN = margin, function(row) {
+  result <- sapply(1:n, function(i) {
+    row <- matrix[i, ]
     rowSum <- sum(row)
     
-    if (rowSum == 0)
-      rep (1/n, n)
-    else
+    if (rowSum == 0) {
+      values <- c(rep(0, i - 1), 1, rep(0, n - i))
+      values
+    } else {
       row / rowSum
+    }
   })
   
   # If we want the result by rows, we have to transpose the matrix,
@@ -27,21 +30,14 @@ normalizeMatrix <- function(matrix, byrow = TRUE) {
 randomStochasticMatrix <- function(n, byrow = TRUE) {
   numRandom <- n * n
   randomNums <- runif(numRandom)
-  zeroProbs  <- runif(n)
   
-  # Adjust to have 0s in each row
-  result <- sapply(1:n, function(i) {
-    zeroProb <- zeroProbs[i]
-    remainProb <- (1 - zeroProb) / numRandom
-    probs <- c(zeroProb, rep(remainProb, numRandom))
-    rowEntries <- sample(c(0, randomNums), n, prob = probs, replace = TRUE)
-    
-    rowEntries
-  })
+  zeroProb <- 0.95
+  remainProb <- (1 - zeroProb) / numRandom
+  probs <- c(zeroProb, rep(remainProb, numRandom))
   
-  if (byrow)
-    result <- t(result)
+  entries <- sample(c(0, randomNums), numRandom, prob = probs, replace = TRUE)
   
+  result <- matrix(entries, n, n, byrow)
   result <- normalizeMatrix(result, byrow)
   
   result

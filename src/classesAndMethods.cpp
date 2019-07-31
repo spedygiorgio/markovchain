25be3980881616b415e356777db3fffe1a487859
ncordon <nacho.cordon.castillo@gmail.com>
2019-07-31 03:16:39 +0200

    Port auxiliary method needed by steadyStates to C++

--- src/classesAndMethods.cpp
+++ src/classesAndMethods.cpp
@@ -1,6 +1,7 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
 using namespace Rcpp;
+using namespace arma;
 
 // check if prob is probability or not
 // [[Rcpp::export(.isProbability)]]
@@ -102,3 +103,71 @@ SEXP lexicographicalSort(SEXP y) {
   
   return(wrap(x));
 }
+
+inline bool approxEqual(const double& a, const double& b) {
+  if (a >= b)
+    return (a - b) <= 1E-7;
+  else
+    return approxEqual(b, a);
+}
+
+inline bool approxEqual(const cx_double& a, const cx_double& b){
+  double x = a.real() - b.real();
+  double y = a.imag() - b.imag();
+  
+  return (x*x - y*y) <= 1E-14;
+}
+
+mat mcEigen(NumericMatrix t) {
+  cx_mat transitionMatrix = as<cx_mat>(t);
+  cx_vec eigvals;
+  cx_mat eigvecs;
+  // 1 + 0i
+  cx_double cx_one(1.0, 0);
+
+  // If transition matrix is hermitian (symmetric in real case), use
+  // more efficient implementation to get the eigenvalues and vectors
+  if (transitionMatrix.is_hermitian()) {
+    vec real_eigvals;
+    eig_sym(real_eigvals, eigvecs, transitionMatrix);
+    eigvals.resize(real_eigvals.size());
+    
+    // eigen values are real, but we need to cast them to complex values
+    // to perform the rest of the algorithm
+    for (int i = 0; i < real_eigvals.size(); ++i)
+      eigvals[i] = cx_double(real_eigvals[i], 0);
+  } else
+    eig_gen(eigvals, eigvecs, transitionMatrix, "balance");
+
+  std::vector<int> whichOnes;
+  std::vector<double> colSums;
+  double colSum;
+  mat real_eigvecs = real(eigvecs);
+  int numRows = real_eigvecs.n_rows;
+    
+  // Search for the eigenvalues which are 1 and store 
+  // the sum of the corresponding eigenvector
+  for (int j = 0; j < eigvals.size(); ++j) {
+    if (approxEqual(eigvals[j], cx_one)) {
+      whichOnes.push_back(j);
+      colSum = 0;
+      
+      for (int i = 0; i < numRows; ++i)
+        colSum += real_eigvecs(i, j);
+      
+      colSums.push_back(colSum);
+    }
+  }
+
+  // Normalize eigen vectors
+  int numCols = whichOnes.size();
+  mat result(numRows, numCols);
+  
+  for (int i = 0; i < numRows; ++i)
+    for (int j = 0; j < numCols; ++j)
+      result(i, j) = real_eigvecs(i, whichOnes[j]) / colSums[j];
+
+
+  return result;
+}
+

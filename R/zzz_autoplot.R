054e68d8ac2741f493ae673d4a0c94839936a6e3
Giorgio Alfredo Spedicato <spedygiorgio@users.noreply.github.com>
2026-08-16 23:07:25 +0200

    fix: declare ggplot2 aesthetic globals

--- R/zzz_autoplot.R
+++ R/zzz_autoplot.R
@@ -1,3 +1,10 @@
+# ggplot2 uses non-standard evaluation for aesthetics. Declare the data-column
+# names used by autoplot.markovchain so R CMD check does not report them as
+# undefined global variables.
+utils::globalVariables(c(
+  "group", "label_x", "label_y", "state", "x", "xend", "y", "yend"
+))
+
 .onLoad <- function(libname, pkgname) {
   if (requireNamespace("ggplot2", quietly = TRUE)) {
     registerS3method(
